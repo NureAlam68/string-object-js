2b2f1a62813af5edd8434954caa6652b36e081de
NureAlam68 <nurealam151068@gmail.com>
2024-09-02 23:04:59 +0600

    string object javascript

---
+++ case.js
@@ -0,0 +1,9 @@
+let subject = 'Chemistry';
+let book = 'CHEmisTRY';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log("Reading more and more");
+}
+else{
+    console.log("Gom debo");
+}
