1cc3e19df9be41ac8b280500c66c74e6ace65a34
NureAlam68 <nurealam151068@gmail.com>
2024-09-02 23:58:58 +0600

    join js

---
+++ join.js
@@ -0,0 +1,13 @@
+//  array to string
+
+let friend = ['Avi', 'Tamim', 'Joy', 'Ismail'];
+
+// console.log(friend.join());
+
+// console.log(friend.join(''));
+
+// console.log(friend.join('|'));
+
+// console.log(friend.join('-'));
+
+console.log(friend.join(' '));
