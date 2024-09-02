ab7920b1343ac803fe683411fac2f61082252e2d
NureAlam68 <nurealam151068@gmail.com>
2024-09-03 00:13:43 +0600

    concat js

---
+++ concat.js
@@ -0,0 +1,16 @@
+let first = 'Nure';
+let last = 'Alam';
+
+// let fullName = first + last;
+
+// let fullName = first + ' ' + last;
+
+// console.log(fullName);
+
+
+
+// for concat
+
+let fullName = first.concat(' ').concat(last);
+
+console.log(fullName);
