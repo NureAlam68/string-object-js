01831a2a7b914a31a215d0f8a6ef234352fcaaff
NureAlam68 <nurealam151068@gmail.com>
2024-09-02 23:52:58 +0600

    split js

---
+++ split.js
@@ -0,0 +1,15 @@
+// string to array
+
+let sentence = 'I am a student';
+
+// console.log(sentence.split());
+
+// console.log(sentence.split(''));
+
+// console.log(sentence.split(' '));
+
+// console.log(sentence.split(','));
+
+// console.log(sentence.split('a'));
+
+console.log(sentence.split(','));
