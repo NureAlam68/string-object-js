02ccc4a83a21270a5f30a6e2cf51449b11ce118c
NureAlam68 <nurealam151068@gmail.com>
2024-09-02 23:18:59 +0600

    case js

--- case.js
+++ case.js
@@ -1,9 +1,31 @@
-let subject = 'Chemistry';
-let book = 'CHEmisTRY';
+//uppercase and lowercase
 
-if(subject.toLowerCase() === book.toLowerCase()){
-    console.log("Reading more and more");
+
+// let subject = 'Chemistry';
+// let book = 'CHEmisTRY';
+
+// if(subject.toLowerCase() === book.toLowerCase()){
+//     console.log("Reading more and more");
+// }
+// else{
+//     console.log("Gom debo");
+// }
+
+
+
+
+
+// trim for space related problem solve
+
+
+
+let drink = '    water';
+let liquid = 'water   ';
+
+
+if(drink.trim() === liquid.trim()){
+    console.log('Lets drink it');
 }
 else{
-    console.log("Gom debo");
+    console.log('Do not try this');
 }
