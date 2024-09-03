7a1471c3d6cbe00401780f09dd462fe807789d79
NureAlam68 <nurealam151068@gmail.com>
2024-09-03 17:12:52 +0600

    keys js

---
+++ keys.js
@@ -0,0 +1,13 @@
+// property array hoye print
+
+let computer = {
+    brand: 'Hp',
+    price: 45000,
+    ssd: '512gb',
+    ram: '16gb',
+    monitor: 'Hp',
+}
+
+let keys = Object.keys(computer);
+
+console.log(keys);
