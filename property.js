68da1711bc401b2d8949eb96c1a3bb9b3212eca8
NureAlam68 <nurealam151068@gmail.com>
2024-09-03 17:00:52 +0600

    property js

---
+++ property.js
@@ -0,0 +1,69 @@
+// object
+
+let subject = {
+    name: 'Chemistry',
+    exam:'10 Sep',
+    chapter: ['First', 'Second'],
+    isDone: true,
+    'fav sub': 'organic',
+    exams: {
+        names: 'First Mid',
+        marks: 10,
+    }
+}
+// console.log(subject);
+
+
+
+
+
+// single property print dot notation
+
+
+// console.log(subject.isDone);
+
+// use variable
+
+// let sub = subject.isDone;
+// console.log(sub);
+
+
+
+
+
+// single property print bracket notation
+
+
+// console.log(subject['name']);
+
+// use variable
+
+// let subName = subject['name'];
+// console.log(subName);
+
+
+
+
+
+// property value change
+
+// subject.name = 'physics';  or
+subject['name'] = 'physics';
+console.log(subject);
+
+
+
+// use variable to access object property
+
+// let keyName = 'name';
+// console.log(subject[keyName]);
+
+
+
+// use variable to change object property
+
+// let keyName = 'name';
+// subject[keyName] = 'physics';
+
+// console.log(subject);
+
