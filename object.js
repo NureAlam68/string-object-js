08d4842f1492ac0849e0541fdf1b0287ce9b28dc
NureAlam68 <nurealam151068@gmail.com>
2024-09-03 15:54:22 +0600

    object js

---
+++ object.js
@@ -0,0 +1,11 @@
+let subject = {
+    name: 'Chemistry',
+    exam:'10 Sep',
+    chapter: ['First', 'Second'],
+    isDone: true,
+    exams: {
+        name: 'First Mid',
+        marks: 10,
+    }
+}
+console.log(subject);
