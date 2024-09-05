8ccf6f9ab36a1a594796f8454da6ceccab6c35f2
NureAlam68 <nurealam151068@gmail.com>
2024-09-05 12:50:28 +0600

    task four

---
+++ task-object/task-four.js
@@ -0,0 +1,18 @@
+// Task-4
+
+// Count the number of properties.
+
+
+
+
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+}
+// console.log(Object.keys(student).length);
+
+let totalKeys = Object.keys(student).length;
+console.log("totalKeys:", totalKeys);
